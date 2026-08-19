f15c3c82d79f0bd076de725a45c02056eda46192
Zoex2304 <zikrifadhiilahahmad@gmail.com>
2026-08-19 21:00:28 +0700

    chore: disable greeting dialog

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { GreetingDialog } from '@/components/greeting-dialog';
+// import { GreetingDialog } from '@/components/greeting-dialog';
 import KBar from '@/components/kbar';
 import AppSidebar from '@/components/layout/app-sidebar';
 import Header from '@/components/layout/header';
@@ -33,7 +33,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
             {children}
             <InfoSidebar side='right' />
             <ChatwootWidget />
-            <GreetingDialog />
+            {/* <GreetingDialog /> */}
           </InfobarProvider>
         </SidebarInset>
       </SidebarProvider>
